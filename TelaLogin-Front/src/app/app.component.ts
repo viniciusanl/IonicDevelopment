c145b0c17e48548789c52989a4336d61426e24ee
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-09-21 11:23:31 -0300

    commit 21/09/2018

--- TelaLogin-Front/src/app/app.component.ts
+++ TelaLogin-Front/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,6 +11,9 @@ import { LoginPage } from '../pages/login/login';
 })
 
 export class MyApp {
+
+  @ViewChild(Nav) nav: Nav;
+
   rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
@@ -19,6 +22,9 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      //this.nav.push(HomePage);
+
     });
   }
 }
