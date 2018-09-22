8c15f989620d3f70ffbdc1f48608b65d8460181c
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-09-22 11:47:12 -0300

    commit 22/09/2018

--- TelaLogin-Front/src/pages/home/home.ts
+++ TelaLogin-Front/src/pages/home/home.ts
@@ -1,18 +1,34 @@
 import { LoginPage } from './../login/login';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  //styleUrls: ['./alert-example.css'],
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public NavParams: NavParams) {}
+  constructor(public navCtrl: NavController, public NavParams: NavParams,
+    //public alertController: AlertController
+    ) {}
 
     //this.navCtrl.setRoot(LoginPage);
+
   ionViewDidLoad(){
     console.log(this.NavParams.get('message'));
   }
+
+  /*async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: console.log(this.NavParams.get('message')),
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }*/
 
 }
